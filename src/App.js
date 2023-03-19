daadf9d9e87a37e4bc3e55646e66adc9a1d9fac0
Subtext Pro <hello@subtext.pro>
2023-03-18 19:21:04 -0700

    form,header,semantic UI

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
 import './index.css';
 import TodoList from './TodoList';
+import Header from './Header';
+import Form from './Form';
 
 
 function App() {
   return (
     <>
+      <Header />
+      <Form />
     <TodoList />
     </>
   )
