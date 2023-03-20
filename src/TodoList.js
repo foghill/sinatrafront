74cb91cd8e2e3beab9f4de0b2bd7b67b947d4d2e
Subtext Pro <hello@subtext.pro>
2023-03-19 19:34:31 -0700

    working

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,40 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Header, Input, List } from 'semantic-ui-react';
+import React, { useState, useEffect } from "react";
+import { Button, Header, Input, List, Dropdown } from "semantic-ui-react";
 
 function TodoList() {
   const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
+  const [newTodo, setNewTodo] = useState("");
   const [editingId, setEditingId] = useState(null);
-  const [editingTitle, setEditingTitle] = useState('');
+  const [editingTitle, setEditingTitle] = useState("");
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [categories, setCategories] = useState([
+    { key: 1, value: "Work", text: "Work" },
+    { key: 2, value: "Personal", text: "Personal" },
+    { key: 3, value: "Shopping", text: "Shopping" },
+    { key: 4, value: "Health", text: "Health" },
+    { key: 5, value: "Travel", text: "Travel" },
+  ]);
 
   useEffect(() => {
-    fetch('http://localhost:9292/todos')
-      .then(response => response.json())
-      .then(data => setTodos(data))
+    fetch("http://localhost:9292/todos")
+      .then((response) => response.json())
+      .then((data) => setTodos(data));
   }, []);
 
   const addTodo = () => {
-    fetch('http://localhost:9292/todos', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: newTodo })
+    fetch("http://localhost:9292/todos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: newTodo, category: selectedCategory }),
     })
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         setTodos([...todos, data]);
-        setNewTodo('');
+        setNewTodo("");
+        setSelectedCategory("");
       });
   };
 
   const updateTodo = (id, newTitle) => {
     fetch(`http://localhost:9292/todos/${id}`, {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ title: newTitle })
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title: newTitle, category: selectedCategory }),
     })
-      .then(response => response.json())
-      .then(data => {
-        const updatedTodos = todos.map(todo => {
+      .then((response) => response.json())
+      .then((data) => {
+        const updatedTodos = todos.map((todo) => {
           if (todo.id === id) {
             return data;
           } else {
@@ -43,17 +52,18 @@ function TodoList() {
         });
         setTodos(updatedTodos);
         setEditingId(null);
-        setEditingTitle('');
+        setEditingTitle("");
+        setSelectedCategory("");
       });
   };
 
-  const deleteTodo = id => {
+  const deleteTodo = (id) => {
     fetch(`http://localhost:9292/todos/${id}`, {
-      method: 'DELETE'
+      method: "DELETE",
     })
-      .then(response => response.json())
-      .then(data => {
-        const filteredTodos = todos.filter(todo => todo.id !== data.id);
+      .then((response) => response.json())
+      .then((data) => {
+        const filteredTodos = todos.filter((todo) => todo.id !== data.id);
         setTodos(filteredTodos);
       });
   };
@@ -65,23 +75,42 @@ function TodoList() {
 
   const cancelEdit = () => {
     setEditingId(null);
-    setEditingTitle('');
+    setEditingTitle("");
   };
 
   return (
-    <div style={{margin: '0 auto', maxWidth: 600, border: '1px solid grey', padding: '20px'}}>
+    <div
+      style={{
+        margin: "0 auto",
+        maxWidth: 600,
+        border: "1px solid grey",
+        padding: "20px",
+      }}
+    >
       <Header as="h1">Todo List</Header>
       <List>
-        {todos.map(todo => (
+        {todos.map((todo) => (
           <List.Item key={todo.id}>
             {editingId === todo.id ? (
               <>
                 <Input
                   type="text"
                   value={editingTitle}
-                  onChange={event => setEditingTitle(event.target.value)}
+                  onChange={(event) => setEditingTitle(event.target.value)}
                 />
-                <Button primary onClick={() => updateTodo(todo.id, editingTitle)}>Save</Button>
+                <Dropdown
+                  placeholder="Select category"
+                  selection
+                  options={categories}
+                  name={selectedCategory}
+                  onChange={(event, data) => setSelectedCategory(data.name)}
+                />
+                <Button
+                  primary
+                  onClick={() => updateTodo(todo.id, editingTitle)}
+                >
+                  Save
+                </Button>
                 <Button onClick={cancelEdit}>Cancel</Button>
               </>
             ) : (
@@ -92,8 +121,12 @@ function TodoList() {
                   onChange={() => {}}
                   disabled
                 />
-                <Button primary onClick={() => editTodo(todo.id, todo.title)}>Edit</Button>
-                <Button negative onClick={() => deleteTodo(todo.id)}>Delete</Button>
+                <Button primary onClick={() => editTodo(todo.id, todo.title)}>
+                  Edit
+                </Button>
+                <Button negative onClick={() => deleteTodo(todo.id)}>
+                  Delete
+                </Button>
               </>
             )}
           </List.Item>
@@ -102,10 +135,19 @@ function TodoList() {
           <Input
             type="text"
             value={newTodo}
-            onChange={event => setNewTodo(event.target.value)}
+            onChange={(event) => setNewTodo(event.target.value)}
             placeholder="Enter new task"
           />
-          <Button positive onClick={addTodo} disabled={!newTodo}>Add</Button>
+          <Dropdown
+            placeholder="Select category"
+            selection
+            options={categories}
+            name={selectedCategory}
+            onChange={(event, data) => setSelectedCategory(data.name)}
+          />
+          <Button positive onClick={addTodo} disabled={!newTodo}>
+            Add
+          </Button>
         </List.Item>
       </List>
     </div>
